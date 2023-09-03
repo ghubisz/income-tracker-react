d31f6e0654c757997382352d58bdf69e05f4f8c9
ghubisz <gab.op.pl@op.pl>
2023-09-03 22:12:06 +0200

    1.2

--- income-tracker/src/components/IncomeList.js
+++ income-tracker/src/components/IncomeList.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import React from 'react';
+import IncomeItem from './IncomeItem';
 
-function IncomeList() {
+function IncomeList({ income, setIncome}) {
+
+  const removeIncome = i => {
+    let temp = income.filter((v, index) => index !== i);
+    setIncome(temp);
+  }
+
+  const sortByDate = (a, b) => {
+    return a.date - b.date;
+  }
+
   return (
-    <div>IncomeList</div>
+    <div className="income-list">
+      {
+        income.sort(sortByDate).map((value, index) => (
+          <IncomeItem 
+            key={index} 
+            income={value} 
+            index={index} 
+            removeIncome={removeIncome}
+          />
+        ))
+      }
+    </div>
   )
 }
 
